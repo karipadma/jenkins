3391af58224ece84cc121186bbe33fc7644ce726
karipadma <41953175+karipadma@users.noreply.github.com>
2026-08-02 21:36:53 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket        = "my-terraform-state"
+    bucket        = "terraform-paddhubucket"
     key           = "terraform.tfstate"
     region         = "us-east-1"
     use_lockfile  = true
